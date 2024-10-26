cedb5d5f84aea22de591536878296d008c0f6cc4
olga_k_qschool <ms.olgasmith.jj@mail.ru>
2024-10-26 19:58:51 +0300

    ad

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,62 @@
 <template>
-  <div>
-    <div>
-      <Container
-        group-name="items"
-        :get-child-payload="(index) => questions[currentQuestion].items[index]"
-        @drop="onDrop"
+  <div class="flex gap-[40px] w-full p-20 mx-auto justify-center">
+ 
+    <div class="w-full xl:w-1/2">
+      <draggable
+        v-model="questions[currentQuestion].items"
+        tag="ul"
+        :animation="300"
       >
-        <Draggable
-          v-for="item in questions[currentQuestion].items"
-          :key="item.id"
-        >
-          <div class="item">
-            {{ item.data }}
-          </div>
-        </Draggable>
-      </Container>
-
+        <template #item="{ element: item }">
+          <li class="item">
+            {{ item.name }}
+          </li>
+        </template>
+      </draggable>
+      <button class="w-full mt-4 p-2 bg-blue-400">
+        Следующий вопрос
+      </button>
     </div>
   </div>
 </template>
 
-<script>
-import { Container, Draggable } from 'vue-dndrop';
-
-export default {
-  name: 'DnD',
-  components: {
-    Container,
-    Draggable,
+<script setup>
+import draggable from 'vuedraggable';
+import { ref } from 'vue';
+const questions = ref([
+  {
+    items: [
+      { id: '1', name: 'Ответ 1' },
+      { id: '2', name: 'Ответ 2' },
+      { id: '3', name: 'Ответ 3' },
+      { id: '4', name: 'Ответ 4' },
+    ],
+    correctOrder: ['1', '2', '3', '4'],
   },
-  data() {
-    return {
-      currentQuestion: 0,
-      errorMessage: '',
-      questions: [
-        {
-          items: [
-            { id: '1', data: 'Ответ 1-1' },
-            { id: '2', data: 'Ответ 1-2' },
-            { id: '3', data: 'Ответ 1-3' },
-            { id: '4', data: 'Ответ 1-4' },
-          ],
-          correctOrder: ['1', '2', '3', '4'],
-        },
-      ],
-    };
+  {
+    items: [
+      { id: '5', name: 'Ответ 5' },
+      { id: '6', name: 'Ответ 6' },
+      { id: '7', name: 'Ответ 7' },
+      { id: '8', name: 'Ответ 8' },
+    ],
+    correctOrder: ['5', '6', '7', '8'],
   },
-  methods: {
-    onDrop(dropResult) {
-      const updatedItems = this.handleDrag(
-        this.questions[this.currentQuestion].items,
-        dropResult,
-      );
-      this.questions[this.currentQuestion].items = updatedItems;
-    },
-    handleDrag(items, dropResult) {
-      const { removedIndex, addedIndex, payload } = dropResult;
-      if (removedIndex === null && addedIndex === null) return items;
-
-      const result = [...items];
-      let itemToAdd = payload;
-
-      if (removedIndex !== null) {
-        itemToAdd = result.splice(removedIndex, 1)[0];
-      }
-
-      if (addedIndex !== null) {
-        result.splice(addedIndex, 0, itemToAdd);
-      }
-      return result;
-    },
+  {
+    items: [
+      { id: '9', name: 'Ответ 9' },
+      { id: '10', name: 'Ответ 10' },
+      { id: '11', name: 'Ответ 11' },
+      { id: '12', name: 'Ответ 12' },
+    ],
+    correctOrder: ['9', '10', '11', '12'],
   },
-};
+]);
+
+const currentQuestion = ref(0);
+
 </script>
+
 
 <style>
 .item {
